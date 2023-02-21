f767a4a2d59fa4f3fcdc1d98fe626a4888b69528
Ryan Wang <i@ryanc.cc>
2023-02-21 15:51:30 +0800

    refactor: move @halo-dev/search-widget to this repo

---
+++ packages/widget/vite.config.ts
@@ -0,0 +1,32 @@
+import { fileURLToPath, URL } from "node:url";
+
+import { defineConfig } from "vite";
+import Vue from "@vitejs/plugin-vue";
+import path from "path";
+
+export default defineConfig({
+  plugins: [Vue()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      vue: "vue/dist/vue.esm-bundler.js",
+    },
+  },
+  build: {
+    outDir: fileURLToPath(
+      new URL("../../src/main/resources/static", import.meta.url)
+    ),
+    emptyOutDir: true,
+    cssCodeSplit: false,
+    lib: {
+      entry: path.resolve(__dirname, "src/index.ts"),
+      formats: ["iife"],
+      name: "SearchWidget",
+      fileName: (format) => `search-widget.${format}.js`,
+    },
+    sourcemap: true,
+  },
+  optimizeDeps: {
+    include: ["vue"],
+  },
+});
