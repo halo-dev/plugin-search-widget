4926bde67f2cc3b6068c1751b5e97af1951ef957
Dacong-wu <2420390517@qq.com>
2023-08-30 16:41:33 +0800

    Supporting manual selection

--- packages/search-widget/src/components/SearchWidget.vue
+++ packages/search-widget/src/components/SearchWidget.vue
@@ -1,9 +1,17 @@
 <script lang="ts" setup>
 import { VModal } from "@halo-dev/components";
-import { ref, watch } from "vue";
+import { ref, watch, onMounted } from "vue";
 import debounce from "lodash.debounce";
 import axios from "axios";
 import type { Result, Hit } from "@/types/model";
+
+onMounted(() => {
+  const htmlElement = document.querySelector("html");
+  if (htmlElement) {
+    const htmlClass = htmlElement.getAttribute("class");
+    if (htmlClass) vElementClass.value = htmlClass;
+  }
+});
 
 const props = withDefaults(
   defineProps<{
@@ -22,7 +30,7 @@ const emit = defineEmits<{
 
 const searchInput = ref<HTMLInputElement | null>(null);
 const keyword = ref("");
-
+const vElementClass = ref("");
 const selectedIndex = ref(0);
 const searchResults = ref<Result>({
   hits: [],
@@ -130,16 +138,30 @@ const onVisibleChange = (visible: boolean) => {
     layer-closable
     @update:visible="onVisibleChange"
   >
-    <div class="bg-slate-50 dark:bg-slate-950">
+    <div
+      class="rounded"
+      :class="{
+        'bg-slate-50': vElementClass === 'light',
+        'bg-slate-950': vElementClass === 'dark',
+      }"
+    >
       <div
         id="search-input"
-        class="border-b border-gray-100 px-4 py-2.5 dark:border-gray-800"
+        class="border-b px-4 py-2.5"
+        :class="{
+          'border-gray-100': vElementClass === 'light',
+          'border-gray-800': vElementClass === 'dark',
+        }"
       >
         <input
           ref="searchInput"
           v-model="keyword"
           placeholder="输入关键词以搜索"
-          class="w-full bg-gray-50 py-1 text-base outline-none dark:bg-gray-950"
+          class="w-full py-1 text-base outline-none"
+          :class="{
+            'bg-gray-50 text-gray-900': vElementClass === 'light',
+            'bg-gray-950 text-gray-100': vElementClass === 'dark',
+          }"
           autocomplete="off"
           autocorrect="off"
           spellcheck="false"
@@ -166,46 +188,104 @@ const onVisibleChange = (visible: boolean) => {
             @click="handleOpenLink(item)"
           >
             <div
-              class="flex flex-col gap-1 rounded-md bg-gray-50 px-2 py-2.5 hover:bg-gray-100 dark:bg-gray-950 dark:hover:bg-gray-900"
+              class="flex flex-col gap-1 rounded-md px-2 py-2.5"
               :class="{
-                '!bg-gray-100 dark:!bg-gray-900': selectedIndex === itemIndex,
+                '!bg-gray-100':
+                  selectedIndex === itemIndex && vElementClass === 'light',
+                'bg-gray-50 hover:bg-gray-100': vElementClass === 'light',
+                'bg-gray-950 hover:bg-gray-900': vElementClass === 'dark',
+                '!bg-gray-900':
+                  selectedIndex === itemIndex && vElementClass === 'dark',
               }"
             >
               <div
                 v-if="item.title"
-                class="text-sm font-semibold text-gray-900 dark:text-gray-100"
+                class="text-sm font-semibold"
+                :class="{
+                  'text-gray-900': vElementClass === 'light',
+                  '!text-gray-100': vElementClass === 'dark',
+                }"
                 v-html="item.title"
               ></div>
               <div
                 v-if="item.content"
-                class="text-xs text-gray-600 dark:text-gray-400"
+                class="text-xs"
+                :class="{
+                  'text-gray-600': vElementClass === 'light',
+                  '!text-gray-400': vElementClass === 'dark',
+                }"
                 v-html="item.content"
               ></div>
             </div>
           </li>
         </ul>
       </div>
-      <div class="border-t border-gray-100 px-4 py-2.5 dark:border-gray-800">
+      <div
+        class="border-t px-4 py-2.5"
+        :class="{
+          'border-gray-100': vElementClass === 'light',
+          'border-gray-800': vElementClass === 'dark',
+        }"
+      >
         <div class="flex items-center justify-end">
-          <span class="mr-1 text-xs text-gray-600">选择</span>
+          <span
+            class="mr-1 text-xs"
+            :class="{
+              'text-gray-600': vElementClass === 'light',
+              'text-gray-400': vElementClass === 'dark',
+            }"
+            >选择</span
+          >
           <kbd
-            class="mr-1 w-5 rounded border p-0.5 text-center text-[10px] text-gray-600 shadow-sm"
+            class="mr-1 w-5 rounded border p-0.5 text-center text-[10px] shadow-sm"
+            :class="{
+              'text-gray-600': vElementClass === 'light',
+              'text-gray-400': vElementClass === 'dark',
+            }"
           >
             ↑
           </kbd>
           <kbd
-            class="mr-5 w-5 rounded border p-0.5 text-center text-[10px] text-gray-600 shadow-sm"
+            class="mr-5 w-5 rounded border p-0.5 text-center text-[10px] shadow-sm"
+            :class="{
+              'text-gray-600': vElementClass === 'light',
+              'text-gray-400': vElementClass === 'dark',
+            }"
           >
             ↓
           </kbd>
-          <span class="mr-1 text-xs text-gray-600">确认</span>
+          <span
+            class="mr-1 text-xs"
+            :class="{
+              'text-gray-600': vElementClass === 'light',
+              'text-gray-400': vElementClass === 'dark',
+            }"
+            >确认</span
+          >
           <kbd
-            class="mr-5 rounded border p-0.5 text-[10px] text-gray-600 shadow-sm"
+            class="mr-5 rounded border p-0.5 text-[10px] shadow-sm"
+            :class="{
+              'text-gray-600': vElementClass === 'light',
+              'text-gray-400': vElementClass === 'dark',
+            }"
           >
             Enter
           </kbd>
-          <span class="mr-1 text-xs text-gray-600">关闭</span>
-          <kbd class="rounded border p-0.5 text-[10px] text-gray-600 shadow-sm">
+          <span
+            class="mr-1 text-xs"
+            :class="{
+              'text-gray-600': vElementClass === 'light',
+              'text-gray-400': vElementClass === 'dark',
+            }"
+            >关闭</span
+          >
+          <kbd
+            class="rounded border p-0.5 text-[10px] shadow-sm"
+            :class="{
+              'text-gray-600': vElementClass === 'light',
+              'text-gray-400': vElementClass === 'dark',
+            }"
+          >
             Esc
           </kbd>
         </div>
