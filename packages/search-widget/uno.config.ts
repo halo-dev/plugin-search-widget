833b2653997a4db584b8f84ba9d01429356ce1d1
Ryan Wang <i@ryanc.cc>
2024-08-08 10:22:03 +0800

    Fix icons
    Signed-off-by: Ryan Wang <i@ryanc.cc>

--- packages/search-widget/uno.config.ts
+++ packages/search-widget/uno.config.ts
@@ -12,7 +12,8 @@ export default defineConfig({
     colors: {
       primary: 'var(--halo-search-widget-primary-color,#4CCBA0)',
       muted: 'var(--halo-search-widget-muted-color,#475569)',
-      content: 'var(--halo-search-widget-content-color,var(--halo-search-widget-color-form-input,#0f172a))',
+      content:
+        'var(--halo-search-widget-content-color,var(--halo-search-widget-color-form-input,#0f172a))',
     },
     borderRadius: {
       base: 'var(--halo-search-widget-base-rounded, 0.4em)',
@@ -23,7 +24,8 @@ export default defineConfig({
       'bg-[var(--halo-search-widget-modal-bg-color,var(--halo-search-widget-color-modal-content-bg,#f8fafc))]',
     'bg-hit':
       'bg-[var(--halo-search-widget-hit-bg-color,var(--halo-search-widget-color-result-item-bg,#fff))]',
-    'bg-base': 'bg-[var(--halo-search-widget-base-bg-color,var(--halo-search-widget-color-modal-content-bg,#fff))]',
+    'bg-base':
+      'bg-[var(--halo-search-widget-base-bg-color,var(--halo-search-widget-color-modal-content-bg,#fff))]',
     'border-divider':
       'border-[var(--halo-search-widget-divider-color,var(--halo-search-widget-color-form-divider,#f1f5f9))]',
     'border-kbd':
@@ -31,4 +33,14 @@ export default defineConfig({
     'shadow-kbd':
       'shadow-[var(--halo-search-widget-kbd-shadow,0px_2px_0px_0px_rgba(0,0,0,0.08))]',
   },
+  safelist: [
+    'i-lucide-arrow-up',
+    'i-lucide-arrow-down',
+    'i-mdi-keyboard-esc',
+    'i-lucide-corner-down-left',
+    'i-lucide-file-text',
+    'i-lucide-history',
+    'i-lucide-loader-circle',
+    'i-lucide-search',
+  ],
 });
