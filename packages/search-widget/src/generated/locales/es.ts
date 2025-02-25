e20b6c88257fc9462e09dafb92de775f36e31e5f
Ryan Wang <i@ryanc.cc>
2025-02-25 11:56:06 +0800

    feat: add i18n supports (#49)

---
+++ packages/search-widget/src/generated/locales/es.ts
@@ -0,0 +1,20 @@
+
+    // Do not modify this file by hand!
+    // Re-generate this file by running lit-localize
+
+    
+    
+
+    /* eslint-disable no-irregular-whitespace */
+    /* eslint-disable @typescript-eslint/no-explicit-any */
+
+    export const templates = {
+      's5e8250fb85d64c23': `Cerrar`,
+'sa84142fc34654130': `Sin resultados de búsqueda`,
+'sad5f82f8364e5a1c': `Introducir palabras clave para buscar`,
+'sb4f1dffbb6be6302': `Borrar`,
+'sb85774dc5d18ff0f': `Confirmar`,
+'sbce70cbdb856635e': `Recientes`,
+'sfc63e31ac7c956ed': `Seleccionar`,
+    };
+  
---
+++ packages/search-widget/src/generated/locales/zh-TW.ts
@@ -0,0 +1,20 @@
+
+    // Do not modify this file by hand!
+    // Re-generate this file by running lit-localize
+
+    
+    
+
+    /* eslint-disable no-irregular-whitespace */
+    /* eslint-disable @typescript-eslint/no-explicit-any */
+
+    export const templates = {
+      's5e8250fb85d64c23': `關閉`,
+'sa84142fc34654130': `沒有搜尋結果`,
+'sad5f82f8364e5a1c': `輸入關鍵字以搜尋`,
+'sb4f1dffbb6be6302': `清空`,
+'sb85774dc5d18ff0f': `確認`,
+'sbce70cbdb856635e': `最近搜尋`,
+'sfc63e31ac7c956ed': `選擇`,
+    };
+  
